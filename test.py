b4130e4d77d3195fd603d7d1a0293f97a8c4cd3d
Ildar DAMINOV <daminovib@gmail.com>
2022-12-17 11:16:57 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,16 @@ import pandas as pd
 import numpy as np 
 import Python_for_literature_review_in_Scopus
 from Python_for_literature_review_in_Scopus import functions,data_classes,research_topic
+from Python_for_literature_review_in_Scopus.data_classes import research_topic
+
 #--------------------------------------------------------------------------
 #                                Case study
 #--------------------------------------------------------------------------
 
 #-------------------------------Input data---------------------------------
+# Define th name of research topic 
+name='Hosting capacity'
+
 # Define the eid of one reference paper 
 reference_paper_eid="2-s2.0-85085924004"
 
@@ -19,6 +24,6 @@ keywords=['hosting capacity',
           'Hosting capacities'
           ]
 
-hosting_capacity=research_topic(reference_paper_eid,keywords)
+hosting_capacity=research_topic(name,reference_paper_eid,keywords)
 
 # ---------------------------- Output data---------------------------------
